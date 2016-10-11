9403dd6c9c874743909e47cbe907cb52f74313c3
Abodu,RollingStone <liudongguo@thinkit.cn>
2016-10-11 16:54:32 +0800

    add list codes

---
+++ algorithms/lists/Solution.java
@@ -0,0 +1,10 @@
+package hackerrank.algorithms.lists;
+
+/**
+ * Created by thinkit on 2016/10/11.
+ */
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("Hello Algorithm Lists");
+    }
+}
